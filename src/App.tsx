2a5f605e051d8cf2ecc0840d4348b7aaa8f7c150
Leo Luna Baima <leobaima01@gmail.com>
2021-08-25 17:04:57 -0300

    Página de admin

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Home } from './Pages/Home'
 import { NewRoom } from "./Pages/NewRoom";
 import { Room } from './Pages/Room';
+import { AdminRoom } from './Pages/AdminRoom'
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" exact component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
